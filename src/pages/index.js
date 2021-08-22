d6d283a87fc447daa017dde58f644b29c607fab6
Carlos Interone <carlos@mostlyclicks.com>
2021-08-22 16:07:24 -0600

    add query to homepage, add props to homebanner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,60 @@
 import * as React from 'react'
+
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs'
+
 import { Layout } from '../components/Layout'
 import { Seo } from '../components/Seo'
 import { HomepageBanner } from '../components/HomepageBanner'
 import { MainContent } from '../components/MainContent'
 
-const Homepage = () => (
-  <Layout isHomepage>
-    <Seo title="Home" />
-    <HomepageBanner />
-    <MainContent />
-  </Layout>
-)
+const Homepage = ({ data }) => {
+
+  if (!data) return null
+  const doc = data.prismicHomepage.data
+
+  return (
+    <Layout isHomepage>
+      <Seo title="Home" />
+      <HomepageBanner 
+        title={RichText.asText(doc.banner_title.raw)}
+        description={RichText.asText(doc.banner_description)}
+        linkUrl={doc.banner_link.url}
+        linkLabel={RichText.asText(doc.banner_link_label.raw)}
+        backgroundUrl={doc.banner_background.url}
+      />
+      <MainContent />
+    </Layout>
+  )  
+}
+
+
+
+export const query = graphql`
+  query Homepage {
+    prismicHomepage {
+      data {
+        banner_title {
+          raw
+        }
+        banner_description {
+          raw
+        }
+        banner_link {
+          url
+          type
+          uid
+        }
+        banner_link_label {
+          raw
+        }
+        banner_background {
+          url
+        }
+      }
+    }
+  }
+`
+
 
 export default Homepage
